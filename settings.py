7420c6be1f83395f289202eac036c6eff119d152
bruna.schroder <bruna.schroder@hp.com>
2021-06-29 15:19:03 -0300

    Fix prod connection string

--- settings.py
+++ settings.py
@@ -11,7 +11,7 @@ DEVELOPMENT = {
 
 PRODUCTION = {
     'app_port': 8080,
-    'db_connection_string': 'postgres://qdccvmmrsbvbkf:22718362e21e56164c25abda03ea289a30559f23e6468a92909793a6f5e1a399@ec2-35-174-35-242.compute-1.amazonaws.com:5432/df9ai60u3rh6f2',
+    'db_connection_string': 'postgresql://qdccvmmrsbvbkf:22718362e21e56164c25abda03ea289a30559f23e6468a92909793a6f5e1a399@ec2-35-174-35-242.compute-1.amazonaws.com:5432/df9ai60u3rh6f2',
     'cache_host': 'ec2-3-215-18-238.compute-1.amazonaws.com',
     'cache_port': 15480,
     'cache_pwd': 'p64ed55c880ee88c02ff15f72903ef122ce4abccbea36708fea2b5c5bfe47efe9',
